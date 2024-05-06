671915e0d76dc6ad3100c652524d1eaab896c974
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-05-06 17:56:04 +0200

    displaying login infos

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,13 @@
 import { auth, googleProvider } from "../config/firebase";
 import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
   const signInWithGoogle = async () => {
     const result = await signInWithPopup(auth, googleProvider);
     console.log(result);
+    navigate("/");
   };
 
   return (
