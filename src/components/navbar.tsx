05d28a64778142a7e6726def1e6a37a17f68c396
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-05-06 18:04:57 +0200

    firebase react hooks to update the user loged in authomaticaly

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,19 @@
-import { Link } from "react-router-dom"
-import { auth } from "../config/firebase"
+import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Navbar = () => {
-    return (
+  const [user] = useAuthState(auth);
+  return (
+    <div>
+      <Link to="/"> Home </Link>
+      <Link to="/login"> Login </Link>
       <div>
-        <Link to="/"> Home </Link>
-        <Link to="/login"> Login </Link>
-        <div>
-          <p>{auth.currentUser?.displayName}</p>
-          <img src={auth.currentUser?.photoURL || ""} width="100" height="100"/>
-        </div>
+        <p>{user?.displayName}</p>
+        <img src={user?.photoURL || ""} width="100" height="100" />
       </div>
-    )
-  }
-  
-  export default Navbar
+    </div>
+  );
+};
+
+export default Navbar;
