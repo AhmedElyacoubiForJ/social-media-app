aa91df18c5d031c86829828b60f9b51f4d1e4007
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-05-08 15:59:10 +0200

    added likes to posts, save to firebase

--- src/pages/home/post.tsx
+++ src/pages/home/post.tsx
@@ -1,11 +1,29 @@
 import { Post as IPost } from "./home";
 
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+
 interface Props {
   post: IPost;
 }
 
 const Post = (props: Props) => {
   const { post } = props;
+  const navigate = useNavigate();
+  const [user] = useAuthState(auth);
+
+  const likesRef = collection(db, "likes");
+
+  const addLike = async () => {
+    await addDoc(likesRef, {
+      postId: post.id,
+      userId: user?.uid
+    });
+    navigate("/");
+  };
+
   return (
     <div>
       <div className="title">
@@ -16,6 +34,7 @@ const Post = (props: Props) => {
       </div>
       <div className="footer">
         <p>@{post.username}</p>
+        <button onClick={addLike}>&#128077;</button>
       </div>
     </div>
   );
